4187777ebbbf81ed12c7e9e6d34f127b275335f5
binhle2302 <150755652+binhle2302@users.noreply.github.com>
2026-03-15 16:13:16 +0700

    new

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("portfolio loaded")
+fetch("sections/projects.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("projects-container").innerHTML = data;
+  });
